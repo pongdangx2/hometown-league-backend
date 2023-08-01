d099d8eae6375f3f37c5fcb6e0b48f4d1c67831d
lkhzz <leta6603@gmail.com>
2023-08-01 20:49:04 +0900

    init

---
+++ src/main/java/me/lkh/hometownleague/HometownLeagueApplication.java
@@ -0,0 +1,13 @@
+package me.lkh.hometownleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometownLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HometownLeagueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/lkh/hometownleague/HometownLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lkh.hometownleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometownLeagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

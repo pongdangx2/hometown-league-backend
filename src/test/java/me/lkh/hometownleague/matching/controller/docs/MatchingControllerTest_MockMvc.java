262647af942863e7539d14d339a4a4062e1198fa
lkhzz <leta6603@gmail.com>
2023-10-03 15:45:54 +0900

    매칭 상세정보 API 문서 개발

--- src/test/java/me/lkh/hometownleague/matching/controller/docs/MatchingControllerTest_MockMvc.java
+++ src/test/java/me/lkh/hometownleague/matching/controller/docs/MatchingControllerTest_MockMvc.java
@@ -7,9 +7,13 @@ import me.lkh.hometownleague.common.response.CommonResponse;
 import me.lkh.hometownleague.common.util.SessionUtil;
 import me.lkh.hometownleague.matching.controller.MatchingController;
 import me.lkh.hometownleague.matching.domain.MatchingListElement;
+import me.lkh.hometownleague.matching.domain.response.MatchingDetailBase;
+import me.lkh.hometownleague.matching.domain.response.MatchingDetailResponse;
+import me.lkh.hometownleague.matching.domain.response.MatchingDetailTeam;
 import me.lkh.hometownleague.matching.service.MatchingService;
 import me.lkh.hometownleague.session.domain.UserSession;
 import me.lkh.hometownleague.session.service.SessionService;
+import me.lkh.hometownleague.team.domain.Team;
 import me.lkh.hometownleague.user.domain.User;
 import org.junit.Rule;
 import org.junit.jupiter.api.DisplayName;
@@ -171,6 +175,119 @@ public class MatchingControllerTest_MockMvc {
                                 fieldWithPath("data[].rankScore").type(JsonFieldType.NUMBER).description("매칭의 팀 점수"),
                                 fieldWithPath("data[].kind").type(JsonFieldType.NUMBER).description("매칭의 팀 종목"),
                                 fieldWithPath("data[].description").type(JsonFieldType.STRING).description("매칭의 팀 설명"),
+                                fieldWithPath("responseCode.code").type(JsonFieldType.STRING).description("응답결과 코드"),
+                                fieldWithPath("responseCode.message").type(JsonFieldType.STRING).description("응답결과 메시지")
+                        )
+                ));
+    }
+
+
+    @DisplayName("매칭 상세정보 조회")
+    @Test
+    void selectMatchingDetail() throws Exception {
+
+        // 세션 관련 Start ======================================================================
+        given(sessionInterceptor.preHandle(any(), any(), any())).willReturn(true);
+
+        String id = "testid@gmail.ccom";
+        String name = "testname";
+        String password = "testPassword";
+        User user = new User(id, name, password);
+        UserSession userSession = new UserSession("spring-session" + SessionUtil.getSessionId(user.getId())
+                , user.getId()
+                , user.getNickname());
+        given(sessionService.getSession(any())).willReturn(userSession);
+        given(sessionService.getUserSession(any())).willReturn(userSession);
+        given(sessionInterceptor.preHandle(any(), any(), any())).willReturn(true);
+        // 세션 관련 End ======================================================================
+
+        MatchingDetailBase matchingDetailBase = new MatchingDetailBase(null, 3, 2, "20230930164443", null, null, "W", "대기", null, null,null, null);
+        Team team = new Team(155, "test-name", null, "테스트 설명", 1500, 1, "neta6603@naver.com");
+        List<User> users = new ArrayList<>();
+        User teamUser = new User("neta6603@naver.com", "테스트닉네임", null, "사용자 설명");
+        users.add(teamUser);
+        MatchingDetailTeam ourTeam = new MatchingDetailTeam(3, "W", "대기", null, team, users);
+        MatchingDetailTeam otherTeam = null;
+        MatchingDetailResponse response = new MatchingDetailResponse(matchingDetailBase, ourTeam, otherTeam);
+        given(matchingService.selectMatchingDetail(any())).willReturn(response);
+
+        String responseContent = objectMapper.writeValueAsString(new CommonResponse<>(response));
+
+        ResultActions resultActions =  this.mockMvc.perform( RestDocumentationRequestBuilders.get("/matching/{matchingRequestId}/detail", 1)
+                .header("cookie", "SESSION=" + userSession.getSessionId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        );
+
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.responseCode").exists())
+                .andExpect(jsonPath("$.responseCode.code").exists())
+                .andExpect(jsonPath("$.responseCode.message").exists())
+                .andExpect((content().json(responseContent)))
+                .andDo(document("matching-select-request-detail",
+                        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
+                        pathParameters(
+                                parameterWithName("matchingRequestId").description("상세정보를 조회할 매칭 요청 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("data").type(JsonFieldType.OBJECT).description("매칭 정보"),
+                                fieldWithPath("data.matchingDetail").type(JsonFieldType.OBJECT).description("매칭 정보"),
+                                fieldWithPath("data.matchingDetail.id").type(JsonFieldType.NUMBER).description("(Optional) 매칭이 잡힌 경우, 잡힌 매칭의 ID").optional(),
+                                fieldWithPath("data.matchingDetail.teamId").type(JsonFieldType.NUMBER).description("매칭을 요청한 팀 ID"),
+                                fieldWithPath("data.matchingDetail.requestTimestamp").type(JsonFieldType.STRING).description("매칭을 요청한 일시 (YYYYMMDDHH24MI형태)"),
+                                fieldWithPath("data.matchingDetail.makeTimestamp").type(JsonFieldType.STRING).description("(Optional) 매칭 성공한 경우 매칭된 일시 (YYYYMMDDHH24MI형태)").optional(),
+                                fieldWithPath("data.matchingDetail.matchTimestamp").type(JsonFieldType.STRING).description("(Optional) 매칭 성공한 경우 결정된 경기 일시 (YYYYMMDDHH24MI형태)").optional(),
+                                fieldWithPath("data.matchingDetail.status").type(JsonFieldType.STRING).description("매칭의 상태 코드"),
+                                fieldWithPath("data.matchingDetail.statusName").type(JsonFieldType.STRING).description("매칭의 상태 코드 명"),
+                                fieldWithPath("data.matchingDetail.matchLatitude").type(JsonFieldType.NUMBER).description("(Optional) 매칭된 경기 장소 위도").optional(),
+                                fieldWithPath("data.matchingDetail.matchLongitude").type(JsonFieldType.NUMBER).description("(Optional) 매칭된 경기 장소 경도").optional(),
+                                fieldWithPath("data.matchingDetail.roadAddress").type(JsonFieldType.STRING).description("(Optional) 매칭된 경기 장소 도로명주소").optional(),
+                                fieldWithPath("data.matchingDetail.jibunAddress").type(JsonFieldType.STRING).description("(Optional) 매칭된 경기 장소 지번주소").optional(),
+/*
+
+    private final Integer teamId;
+    private final String status;
+    private final String statusName;
+    private final String acceptTimestamp;
+    private final Team team;
+    private final List<User> players;
+ */
+//                                fieldWithPath("data[].description").type(JsonFieldType.STRING).description("매칭의 팀 설명"),
+                                fieldWithPath("data.ourTeam").type(JsonFieldType.OBJECT).description("매칭과 관련된 우리팀 정보"),
+                                fieldWithPath("data.ourTeam.status").type(JsonFieldType.STRING).description("매칭 관련 우리팀 상태"),
+                                fieldWithPath("data.ourTeam.statusName").type(JsonFieldType.STRING).description("매칭 관련 우리팀 상태명"),
+                                fieldWithPath("data.ourTeam.team").type(JsonFieldType.OBJECT).description("매칭 관련 우리팀 상세 정보"),
+                                fieldWithPath("data.ourTeam.team.id").type(JsonFieldType.NUMBER).description("팀 ID"),
+                                fieldWithPath("data.ourTeam.team.name").type(JsonFieldType.STRING).description("팀명"),
+                                fieldWithPath("data.ourTeam.team.ownerId").type(JsonFieldType.STRING).description("소유주 ID"),
+                                fieldWithPath("data.ourTeam.team.description").type(JsonFieldType.STRING).description("팀 상세정보"),
+                                fieldWithPath("data.ourTeam.team.rankScore").type(JsonFieldType.NUMBER).description("팀 점수"),
+                                fieldWithPath("data.ourTeam.team.kind").type(JsonFieldType.NUMBER).description("팀 종목"),
+                                fieldWithPath("data.ourTeam.team.ciPath").type(JsonFieldType.STRING).description("(Optional)팀로고 경로").optional(),
+                                fieldWithPath("data.ourTeam.players").type(JsonFieldType.ARRAY).description("팀 소속 선수"),
+                                fieldWithPath("data.ourTeam.players[].id").type(JsonFieldType.STRING).description("선수 ID"),
+                                fieldWithPath("data.ourTeam.players[].nickname").type(JsonFieldType.STRING).description("선수 닉네임"),
+                                fieldWithPath("data.ourTeam.players[].description").type(JsonFieldType.STRING).description("선수 소개글"),
+                                fieldWithPath("data.ourTeam.players[].ciPath").type(JsonFieldType.STRING).description("(Optional) 선수로고 경로").optional(),
+                                fieldWithPath("data.otherTeam").type(JsonFieldType.OBJECT).description("(Optional) 매칭과 관련된 상대팀 정보").optional(),
+                                fieldWithPath("data.otherTeam.status").type(JsonFieldType.STRING).description("매칭 관련 우리팀 상태"),
+                                fieldWithPath("data.otherTeam.statusName").type(JsonFieldType.STRING).description("매칭 관련 우리팀 상태명"),
+                                fieldWithPath("data.otherTeam.team").type(JsonFieldType.OBJECT).description("매칭 관련 우리팀 상세 정보"),
+                                fieldWithPath("data.otherTeam.team.id").type(JsonFieldType.NUMBER).description("팀 ID"),
+                                fieldWithPath("data.otherTeam.team.name").type(JsonFieldType.STRING).description("팀명"),
+                                fieldWithPath("data.otherTeam.team.ownerId").type(JsonFieldType.STRING).description("소유주 ID"),
+                                fieldWithPath("data.otherTeam.team.description").type(JsonFieldType.STRING).description("팀 상세정보"),
+                                fieldWithPath("data.otherTeam.team.rankScore").type(JsonFieldType.NUMBER).description("팀 점수"),
+                                fieldWithPath("data.otherTeam.team.kind").type(JsonFieldType.NUMBER).description("팀 종목"),
+                                fieldWithPath("data.otherTeam.team.ciPath").type(JsonFieldType.STRING).description("(Optional)팀로고 경로").optional(),
+                                fieldWithPath("data.otherTeam.players").type(JsonFieldType.ARRAY).description("팀 소속 선수"),
+                                fieldWithPath("data.otherTeam.players[].id").type(JsonFieldType.STRING).description("선수 ID"),
+                                fieldWithPath("data.otherTeam.players[].nickname").type(JsonFieldType.STRING).description("선수 닉네임"),
+                                fieldWithPath("data.otherTeam.players[].description").type(JsonFieldType.STRING).description("선수 소개글"),
+                                fieldWithPath("data.otherTeam.players[].ciPath").type(JsonFieldType.STRING).description("(Optional) 선수로고 경로").optional(),
                                 fieldWithPath("responseCode.code").type(JsonFieldType.STRING).description("응답결과 코드"),
                                 fieldWithPath("responseCode.message").type(JsonFieldType.STRING).description("응답결과 메시지")
                         )
